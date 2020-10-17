2fc089bbc91941f0fa11acbdd3e7ca0b335786a7
FionaAvila <fionaavila15@gmail.com>
2020-10-17 13:47:48 +0700

    Initial Commit

---
+++ src/if1/pkg10119013/latihan4/kambing/Kambing.java
@@ -0,0 +1,33 @@
+/*
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+
+package if1.pkg10119013.latihan4.kambing;
+
+/**
+ *
+ * @author Fiona Avila
+ */
+
+public class Kambing {
+
+    public void tambahKambing(){
+        //deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah : " + 
+                            jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
